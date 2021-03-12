59ed828d0417cfaf59a3dc8caf7b30ede13aba4b
pranav5601 <vachhanipranav61@gmail.com>
2021-03-12 09:18:32 +0530

    Pranav: added App class and created packages.

--- app/src/main/java/com/example/budgetbuddy/activities/ActMain.java
+++ app/src/main/java/com/example/budgetbuddy/activities/ActMain.java
@@ -1,14 +1,16 @@
-package com.example.budgetbuddy;
+package com.example.budgetbuddy.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.budgetbuddy.R;
+
+public class ActMain extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.act_main);
     }
 }
